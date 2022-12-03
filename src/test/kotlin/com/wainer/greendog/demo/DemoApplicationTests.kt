18656643ca738f8a8bd5ae30d887cab42b03bbd1
Rafael Wainer <rafael.wainer@gmail.com>
2022-12-03 14:19:38 -0300

    Initial commit

---
+++ src/test/kotlin/com/wainer/greendog/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wainer.greendog.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
